41e6534cdc8f5829525737d226d66db4ddb3fb9e
ariko <philemonariko@gmail.com>
2024-03-12 01:09:41 +0300

    k

--- app/components/OffcanvasToggler.tsx
+++ app/components/OffcanvasToggler.tsx
@@ -1,18 +1,22 @@
 "use client"
 
+import Image from "next/image"
 import { Button } from "react-bootstrap"
 
 
 export function OffcanvasToggler({handleShow}:{handleShow: any }){
 
   return(
-    <Button variant='none' className='offcanvas-toggler d-inline-block d-md-none me-2 px-0 '>
-      <img
-        src="/icons/menu.svg"
-        width ="30px"
-        height = "30px"
-        alt="menu icon"
-        onClick = { handleShow }
+    <Button
+      variant='none'
+      className='offcanvas-toggler d-inline-block d-md-none me-2 px-0 '
+      onClick = { handleShow }
+    >
+      <Image
+        src="/icons/close.svg"
+        width = {30}
+        height = {30}
+        alt = "menu icon"
       />
     </Button>
   )
