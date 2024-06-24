2632e19abd15c4d42323d26eda3a67875508889d
Philemon <philemonariko@gmail.com>
2024-06-25 01:24:16 +0300

    d

--- app/components/LoanCard.tsx
+++ app/components/LoanCard.tsx
@@ -137,7 +137,7 @@ function LoanPaymentForm({loan, setStatus}: any) {
     const day = dateObj.getDate();
     const month = dateObj.getMonth() + 1
     const year = dateObj.getFullYear()
-    return `${day}-${month}-${year}`
+    return `${month}-${day}-${year}`
   }
   return (
     <Form onSubmit = {handleSubmit}>
