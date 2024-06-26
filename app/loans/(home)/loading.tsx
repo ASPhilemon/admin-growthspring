023d9b2140a1c6bbf323c9f747b2823f23ca134c
Philemon <philemonariko@gmail.com>
2024-06-26 13:45:35 +0300

    iu

--- app/loans/(home)/loading.tsx
+++ app/loans/(home)/loading.tsx
@@ -7,8 +7,8 @@ export default function Loading(){
     <div className="px-md-5 px-3">
       <div className="d-flex align-items-center py-3">
         <Placeholder className = "me-3 col-12" as = "div" animation="glow" >
-          <Placeholder size="lg" className="me-2 py-3"  xs = {3}/>
-          <PlaceholderButton size="sm" variant="primary" xs = {3}/>
+          <Placeholder className="me-2 rounded-1 py-3"  xs = {3}/>
+          <PlaceholderButton size="sm" className = "py-1" variant="primary" xs = {3}/>
         </Placeholder>
       </div>
       <Placeholder animation="glow" >
@@ -37,19 +37,19 @@ function LoanCardSkeleton(){
           <Placeholder animation="glow" >
             <Placeholder xs ={0} className = "p-3 rounded-circle me-2" />
           </Placeholder>
-          <Placeholder className = "me-2 col-7" animation="glow" >
-            <Placeholder  xs = {12} />
+          <Placeholder className = "me-2 col-6" animation="glow" >
+            <Placeholder className="rounded-pill"  xs = {12} />
           </Placeholder>
-          <Placeholder className = "ms-auto col-4" animation="glow" >
-            <Placeholder xs={4} className = "ms-auto me-2 d-block rounded-1"/>
+          <Placeholder className = "ms-auto col-5" animation="glow" >
+            <Placeholder xs={7} className = "ms-auto me-3 d-block rounded-pill"/>
           </Placeholder>
         </div>
 
         <div className="d-flex align-items-center justify-content-between">
           <div className="w-75">
             <Placeholder xs={12} animation="glow">
-              <Placeholder xs={6} className = "mb-3 d-block"/>
-              <Placeholder xs={6} className = "mb-2 d-block"/>
+              <Placeholder xs={6} className = "mb-3 rounded-pill d-block"/>
+              <Placeholder xs={6} className = "mb-2 rounded-pill d-block"/>
             </Placeholder>
           </div>
 
