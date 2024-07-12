db3fca07cd277de61320961d0324bce34aaa9ae6
Ariko <philemonariko@gmail.com>
2024-07-11 17:35:45 -0700

    fixed bug in middleware

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function middleware(req: NextRequest){
   const user = await getUser()
   if (!user) return NextResponse.redirect('https://auth.growthspringers.com/signin?redirectURI=https://admin.growthspringers.com')
-  if (!(user && user.isAdmin)) return NextResponse.redirect('https://auth.growthspringers.com/signin?redirectURI=https://admin.growthspringers.com')
+  if (user.isAdmin ==="false") return NextResponse.redirect('https://auth.growthspringers.com/signin?redirectURI=https://growthspringers.com')
 
   let reqHeaders = new Headers(req.headers)
   reqHeaders.set('user-id', user.id)
@@ -23,6 +23,7 @@ async function getUser(){
   if (!token) return null
   try{
     const {id, fullName, isAdmin} : any = (await jose.jwtVerify(token, new TextEncoder().encode('top_secret_xyz123') )).payload
+    
     return {id, fullName, isAdmin}
   } catch(err){
     console.log(err)
